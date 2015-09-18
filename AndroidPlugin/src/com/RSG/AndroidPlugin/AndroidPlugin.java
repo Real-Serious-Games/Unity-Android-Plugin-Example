ff71c89bbfe86e74b2a0f3e791c4404e3e105e39
Ashley Davis <ashley.davis@realseriousgames.com>
2015-09-18 13:06:15 +1000

    Fleshed out the Android plugin to get the battery level using native Android APIs.

--- AndroidPlugin/src/com/RSG/AndroidPlugin/AndroidPlugin.java
+++ AndroidPlugin/src/com/RSG/AndroidPlugin/AndroidPlugin.java
@@ -1,11 +1,22 @@
 package com.RSG.AndroidPlugin;
 
 import android.os.Environment;
+import android.os.BatteryManager;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.Context;
 
 public class AndroidPlugin
 {
-    public static float GetBatteryPct()
+    public static float GetBatteryPct(Context context)
     {
-    	return 100f;
+ 		IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+		Intent batteryStatus = context.registerReceiver(null, ifilter);
+
+    	int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+		int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+		float batteryPct = level / (float)scale;
+		return batteryPct;
     }
 }
