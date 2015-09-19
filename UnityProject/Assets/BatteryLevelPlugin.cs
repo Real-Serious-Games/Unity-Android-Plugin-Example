f8528ec706d1f060eaf88f961aa39773ba591b60
Rory Dungan <rorydungan@gmail.com>
2015-09-19 13:51:33 +1000

    Updated Unity project to reflect changes to the Java code

--- UnityProject/Assets/BatteryLevelPlugin.cs
+++ UnityProject/Assets/BatteryLevelPlugin.cs
@@ -4,17 +4,22 @@ using System.Collections;
 
 public class BatteryLevelPlugin 
 {
+    public BatteryLevelPlugin()
+    {
+        
+    }
+    
     public static float GetBatteryLevel()
     {
         if (Application.platform == RuntimePlatform.Android)
         {
-            using (var androidPlugin = new AndroidJavaClass("com.RSG.AndroidPlugin.AndroidPlugin"))
+            using (var javaUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
             {
-                using (var javaUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+                using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
                 {
-                    using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+                    using (var androidPlugin = new AndroidJavaObject("com.RSG.AndroidPlugin.AndroidPlugin", currentActivity))
                     {
-                        return androidPlugin.CallStatic<float>("GetBatteryPct", currentActivity);
+                        return androidPlugin.Call<float>("GetBatteryPct");
                     }
                 }
             }
